6363060234310f40ae4abf97258d91d9dc2a5940
Gloria <ubahgloria16@gmail.com>
2023-11-24 03:17:10 +0100

    Feat: Added trial notification

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const notification = document.querySelector(".notification_div");
 const menuPopup = document.querySelector(".menu_popup");
 const userProfile = document.querySelector(".user_profile");
 const userInitials = document.querySelector(".user_initials");
+const planSelection = document.querySelector(".plan_selection");
+const exitIcon = document.querySelector(".exit_icon");
+const exitIconDesktop = document.querySelector(".exit_icon_desktop");
 
 bellDiv.addEventListener("click", () => {
      notification.classList.toggle("show_notification");
@@ -15,4 +18,12 @@ userProfile.addEventListener("click", () => {
      menuPopup.classList.toggle("show_menu_popup");
      userInitials.classList.toggle("show_border");
      userProfile.classList.toggle("user_profile_active");
+});
+
+exitIcon.addEventListener("click", () => {
+     planSelection.classList.add("remove_trial");
+});
+
+exitIconDesktop.addEventListener("click", () => {
+     planSelection.classList.add("remove_trial");
 });
