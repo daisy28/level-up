b9f4acd812356eec9686657a5eac52ea828bcc56
Gloria <ubahgloria16@gmail.com>
2023-11-24 02:30:24 +0100

    Feat: Navigation done

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Your javascript goes here
+
+const bellDiv = document.querySelector(".bell_div");
+const notification = document.querySelector(".notification_div");
+const menuPopup = document.querySelector(".menu_popup");
+const userProfile = document.querySelector(".user_profile");
+const userInitials = document.querySelector(".user_initials");
+
+bellDiv.addEventListener("click", () => {
+     notification.classList.toggle("show_notification");
+     bellDiv.classList.toggle("notification_active");
+});
+
+userProfile.addEventListener("click", () => {
+     menuPopup.classList.toggle("show_menu_popup");
+     userInitials.classList.toggle("show_border");
+     userProfile.classList.toggle("user_profile_active");
+});
